27e7a4e9ec8a742c9fafb2c75c8e61ddb0128803
tugaysert <mustafatugaysert@gmail.com>
2022-08-12 17:25:50 +0300

    -

---
+++ etiyaCamp4Spring-main pair 4/etiyaCamp4Spring-main/src/main/java/com/etiya/northwind/dataAccess/abstracts/CartItemsRepository.java
@@ -0,0 +1,15 @@
+package com.etiya.northwind.dataAccess.abstracts;
+
+import com.etiya.northwind.entities.concretes.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CartItemsRepository extends JpaRepository<CartItem, Integer> {
+
+    @Query("select c from CartItem c where c.cart.cartId =:cartId")
+    public List<CartItem> getCartItemByCartId(@Param("cartId") int cartId);
+
+}
